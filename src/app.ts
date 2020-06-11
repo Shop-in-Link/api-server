fd9dc4dce5dc68f07d29bd8461d88d21d1190290
Sangwoo <coro101@naver.com>
2020-06-12 03:54:39 +0900

    Add logic to connect to MongoDB server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 import { AppRouter } from "./AppRouter";
 
@@ -6,6 +8,15 @@ const app = express();
 
 app.use(AppRouter.getInstance())
 
-app.listen(3000, (): void => {
-    console.log("Listening on port 3000.");
-});
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-mnzxu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then((): void => {
+        app.listen(3000, (): void => {
+            console.log("Listening on port 3000.");
+        });
+    })
+    .catch((): void => {
+        console.log("Failed to start the server!");
+    });
