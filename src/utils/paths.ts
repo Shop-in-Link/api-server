2d30d1bfd0432e78ed651e7524d38c58af9950fa
Sangwoo <coro101@naver.com>
2020-06-23 08:29:57 +0900

    Add path function for JSON Web Token keys

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,4 +1,8 @@
 import path from 'path';
+import 'dotenv/config';
+
+export const jwtSecretPath = path.join(__dirname, '..', '..', process.env.JWT_SECRET!);
+export const jwtPublicPath = path.join(__dirname, '..', '..', process.env.JWT_PUBLIC!);
 
 export const graphqlTypesPath = path.join(__dirname, '..', '..', 'src', 'graphql', '**/*.graphql');
 export const graphqlResolversPath = path.join(__dirname, '..', 'graphql', '**/resolver.js');
